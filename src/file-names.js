5e3c245738d9192dfcbcf59567ce1fff691c2b6c
Gunsnfnr <a.j.p.010203@gmail.com>
2023-12-20 17:10:53 +0300

    feat: add file-names task

--- src/file-names.js
+++ src/file-names.js
@@ -1,7 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * There's a list of file, since two files cannot have equal names,
+ * There's a list of files, since two files cannot have equal names,
  * the one which comes later will have a suffix (k),
  * where k is the smallest integer such that the found name is not used yet.
  *
@@ -15,9 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let resArr = [];
+  names.forEach(element => {
+    if (resArr.includes(element)) {
+      let copyElement = element + '(1)';
+      if (resArr.includes(copyElement)) {
+        const regex = /\(\d{1,}\)$/;
+        const ending = copyElement.match(regex);
+        const index = copyElement.slice(copyElement.indexOf('(') + 1, copyElement.indexOf(')'));
+        copyElement = element + '(' + (+index + 1) + ')';
+        resArr.push(copyElement);
+        // }
+      } else {
+        resArr.push(copyElement);
+      }
+    } else {
+      resArr.push(element);
+    }
+  });
+  return resArr;
 }
 
 module.exports = {
