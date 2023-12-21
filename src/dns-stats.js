bd7a92b5e48f5ad150fe06d1a00a988dbe0a4e85
Gunsnfnr <a.j.p.010203@gmail.com>
2023-12-21 23:36:06 +0300

    feat add tasks 8-10

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
+  
+  const obj = {};
+  domains.forEach(element => {
+    const splitted = element.split('.');
+    const firstDns = '.' + splitted[splitted.length - 1];
+    if (firstDns in obj) {
+      obj[firstDns] = obj[firstDns] + 1;
+    } else {
+      obj[firstDns] = 1;
+    }
+    let acc = firstDns;
+    for (let i = splitted.length - 2; i >= 0; i -= 1) {
+      acc += '.' + splitted[i];
+      if (acc in obj) {
+        obj[acc] = obj[acc] + 1;
+      } else {
+        obj[acc] = 1;
+      }
+    }
+  });
+  return obj;
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  // throw new NotImplementedError('Not implemented');
+
+  let result = '';
+  let acc = '';
+  let currQuantity = 1;
+  for (let i = 0; i <= str.length; i += 1) {
+    if (str[i] === str[i + 1]) {
+      acc = str[i];
+      currQuantity += 1;
+    } else {
+      if (acc === '') {
+        result += str[i];
+      } else {
+        result += currQuantity + acc;
+        currQuantity = 1;
+        acc = '';
+      }
+    }
+  }
+
+  return result;
+  console.log('result: ', result);
 }
+
 
 module.exports = {
   encodeLine
