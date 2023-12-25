c18bc19d756f1d74283355d2e6491f1a7f8c8db3
Gunsnfnr <a.j.p.010203@gmail.com>
2023-12-25 12:46:05 +0300

    feat: add task 18: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if (typeof String(value) !== 'string') return false;
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (typeof position !== 'number' || position > this.chain.length || position < 1) {
+      this.chain = [];
+      throw new Error("You can\'t remove incorrect link!");
+    };
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    res = '';
+    visual = this.chain.reduce((acc, currElem) => acc === null ? currElem : acc + '~~' + currElem, null);
+    this.chain = [];
+    return visual;
   }
 };
 
